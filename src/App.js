646efad7512191f454710e5e800e4985277bd4b5
Tim <luismuraya11@gmail.com>
2025-03-02 19:25:34 -0500

    Added the header with its buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
 
-export default function App() {
+function ControlButtons() {
   return (
-    <>
-      <h1 id="hi">Hello World</h1>;
-    </>
+    <div  id="buttonDiv">
+      <button type="button" className="btn btn-outline-primary btn-sm custom-btn">Deal 5</button>
+      <button type="button" className="btn btn-outline-secondary custom-btn">Deal 7</button>
+      <button type="button" className="btn btn-outline-success custom-btn">Reset</button>
+      <button type="button" className="btn btn-outline-danger custom-btn">Toss</button>
+      <button type="button" className="btn btn-outline-warning custom-btn">Wildcard</button>
+      <button type="button" className="btn btn-outline-info custom-btn">Regroup</button>
+    </div>
   );
 }
+
+function App() {
+  return (
+    <header className='d-flex align-items-center flex-column container'>
+      <h1 id="hi" className='text-center'>Hello World</h1>;
+      <img src="https://deckofcardsapi.com/static/img/back.png" alt='back turned card' id='backCard'/>
+      <ControlButtons />
+    </header>
+  );
+}
+export default App;
