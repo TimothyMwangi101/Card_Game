23b199491ebdeb7f2fd2cb8b6f31d87f5aad3ea7
Tim <luismuraya11@gmail.com>
2025-03-05 15:29:24 -0500

    - Refactored App.js to adapt it to class Card and make it more modular. - Removed resetDraw() from App.js - Implroved styling - Added cards folder to git ignore

--- src/App.js
+++ src/App.js
@@ -2,54 +2,39 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Card from './Card';
 
+const Cards = createCards();
 
-const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-const suits = ["C", "D", "H", "S"];
-let cards = [];
-
-for (let i = 0; i < values.length; i++) {
-  for (let s = 0; s < suits.length; s++) {
-    const value = values[i] + suits[s];
-    cards.push({
-      VALUE: value,
-      _ISDRAWN: true,
-      get ISDRAWN() {
-        return this._ISDRAWN;
-      },
-      set ISDRAWN(value) {
-        this._ISDRAWN = value;
-      },
-      PATH: `./cards/${value}.png`,
-      DELETED: false
-    })
+function createCards() {
+  const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  const suits = ["C", "D", "H", "S"];
+  let arr = [];
+  for (let i = 0; i < values.length; i++) {
+    for (let s = 0; s < suits.length; s++) {
+      arr.push(new Card(values[i] + suits[s]));
+    }
   }
+  return arr;
 }
 
 function fetchCard(index) {
-  const card = cards[index];
+  const card = Cards[index];
   //if i cant draw or is deleted
-  if (!card.ISDRAWN || !card.DELETED)
-    return fetchCard(Math.floor(Math.random() * cards.length));
-  card.ISDRAWN = false;
+  if (card.getIsDeleted() || card.getIsDrawn())
+    return fetchCard(Math.floor(Math.random() * Cards.length));
+  card.setIsDrawn(true);
   return card;
 }
 
 function resetDraw() {
-  cards.forEach(card => {
-    card.ISDRAWN = true;
+  Cards.forEach(card => {
+    card.setIsDeleted(false);
+    card.setIsDrawn(false);
   });
-}
-function remainingCards() {
-  let rem = 0;
-  cards.forEach(c => {
-    if (c.ISDRAWN && !c.DELETED) rem++;
-  });
-  return rem;
 }
 
 function ControlButtons({ props }) {
   return (
-    <div id="buttonDiv">
+    <div id="buttonDiv" className='d-flex flex-wrap flex-row'>
       <button type="button" className="btn btn-outline-primary btn-sm custom-btn" onClick={() => props.handleClick(5)}>Deal 5</button>
       <button type="button" className="btn btn-outline-secondary custom-btn" onClick={() => props.handleClick(7)}>Deal 7</button>
       <button type="button" className="btn btn-outline-success custom-btn" onClick={() => props.handleReset()}>Reset</button>
@@ -60,44 +45,70 @@ function ControlButtons({ props }) {
   );
 }
 
+function DisplayCards({ props }) {
+  if (props.showCardDiv) {
+    return (
+      <div id="cardDiv" className='d-flex align-items-center flex-column container'>
+        <div className='row justify-content-center flex-row flex-wrap'>
+          {
+            props.cardState.map((c) => (
+              <div className='col-sm'>
+                <img
+                  id={`Card:${c.getValue()}`}
+                  src={c.getPath()}
+                  alt={`A Card of value:${c.getValue()}`}
+                  className={"card"}
+                />
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    );
+  }
+  return (
+    <></>
+  )
+}
+
 function Header() {
   const [showCardDiv, setShowCardDiv] = useState(false);
-  const [card, setCard] = useState([]);
-  const [cardCount, setCardCount] = useState(remainingCards());
-  const [clicked, setClicked] = useState("");
+  const [cardState, setCard] = useState([]);
+  const [cardCount, setCardCount] = useState(52);
+  const [picked, setPicked] = useState(false);
 
   function handleReset() {
     setCard([]);
     setShowCardDiv(false);
     resetDraw();
-    setCardCount(remainingCards());
+    setCardCount(52);
   }
 
-  function picked() {
-    if (clicked) {
+  function handlePicked() {
+    setPicked(true);
 
-    }
+    // if (clicked) {
+
+    // }
   }
 
   function handleClick(num) {
     if (num === 7 || num === 5) handleReset();
 
     if (cardCount > 0) {
-      let index = Math.floor(Math.random() * cards.length);
       const cardsToAppend = [];
 
       for (let i = 0; i < num; i++) {
+        const index = Math.floor(Math.random() * Cards.length);
         const c = fetchCard(index);
         cardsToAppend.push(c);
       }
 
       //1 gets appended, 5/7 overwrite
-      setCard((num === 1) ? prev => [...prev, cardsToAppend] : cardsToAppend)
+      setCard((num === 1) ? prev => [...prev, ...cardsToAppend] : cardsToAppend)
+      setCardCount(prev => Math.max(prev - cardsToAppend.length, 0));
+      setShowCardDiv(true);
     }
-    console.log(card);
-
-    setShowCardDiv(true);
-    setCardCount(remainingCards());
   }
 
   return (
@@ -117,30 +128,9 @@ function Header() {
         Remaining Cards: {cardCount === 0 ? "No Cards Remaining" : cardCount}
       </h3>
 
-      {
-        showCardDiv &&
-        (
-          <div id="cardDiv" className='d-flex align-items-center flex-column container'>
-            <div className='row'>
-              {
-                card.map((c) => (
-                  <div className='col-sm'>
-                    <img
-                      id={`Card${c.VALUE}`}
-                      src={c.PATH}
-                      alt={`A Card of value:${c.VALUE}`}
-                      className={clicked + "card"}
-                      onClick={picked}
-                    />
-                  </div>
-                ))
-              };
-            </div>
-          </div>
-        )
-      };
+      <DisplayCards props={{ showCardDiv, cardState, picked }} />
     </header>
-  )
+  );
 }
 
 function App() {
--- src/Card.js
+++ src/Card.js
@@ -1,25 +1,70 @@
+/**
+ * Represents a card with a value, an image path, and state bool flags.
+ */
 export default class Card {
+    /** @type {string} The suit and value of a card */
     #value;
+
+    /** @type {string} The file path to the card's image. */
     #path;
+
+    /** @type {boolean} Whether the card has been drawn. */
     #isDrawn;
+
+    /** @type {boolean} Whether the card has been deleted. */
     #isDeleted;
 
+    /**
+     * Creates a new Card.
+     * @param {string} value - The suit and value of a card.
+    */
     constructor(value) {
         this.#value = value;
         this.#path = `./cards/${value}.png`;
         this.#isDrawn = false;
         this.#isDeleted = false;
     }
-
+    /**
+     * Value getter 
+     * @returns {string} The suit and value of a card
+    */
     getValue() { return this.#value; }
 
+    /**
+     * Path getter
+     * @returns {string} The file path to the card's image.
+    */
     getPath() { return this.#path; }
 
+    /** 
+     * isDrawn getter 
+     * @returns {boolean} `true` if drawn
+    */
     getIsDrawn() { return this.#isDrawn; }
 
+    /**
+     * isDeleted getter
+     * @returns {boolean} `true` if deleted
+    */
     getIsDeleted() { return this.#isDeleted; }
 
+    /** 
+     * isDrawn setter 
+     * @param {boolean} isDrawn new state
+    */
     setIsDrawn(isDrawn) { this.#isDrawn = isDrawn; }
 
+    /**
+     * isDrawn setter
+     * @param {boolean} isDeleted new state
+    */
     setIsDeleted(isDeleted) { this.#isDeleted = isDeleted; }
+
+    /**
+     * String representation of the card
+     * @returns {string} the card fields
+    */
+    toString() {
+        return `Value: "${this.#value}", Path: "${this.#path}", IsDrawn: ${this.#isDrawn}, IsDeleted: ${this.#isDeleted}`;
+    }
 }
